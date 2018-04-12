a01d552187a383ffeb7c8ff664459ca0f1f22a9a
Mitchell Grout <mjg44@students.waikato.ac.nz>
2018-04-12 13:01:38 +1200

    Tidy main

--- src/trail/main.cpp
+++ src/trail/main.cpp
@@ -10,11 +10,18 @@
 #include "maw32_trail.cpp"
 #include <utility>
 
+// Read a byte from /dev/urandom
+static inline uint8_t csprng_rand()
+{
+    static FILE *handle = fopen("/dev/urandom", "r");
+    return getc(handle);
+}
+
 // Determine if an input differential is viable
 static inline bool is_viable(uint8_t *W, const size_t rounds, const float l2pthresh,
                              size_t t, size_t ctr)
 {
-    if (t >= 16) return ctr >= 0;   // 2/8
+    if (t >= rounds) { printf("%zu\n", ctr); return true; }
     uint8_t w0 = sigma0(W[t-3]),
             w1 = sigma1(W[t-8]);
     for (uint8_t t1: propagate_add(w0, w1, l2pthresh))
@@ -26,14 +33,6 @@ static inline bool is_viable(uint8_t *W, const size_t rounds, const float l2pthr
     return false;
 }
 
-// Successor of an array of bytes
-static inline bool succ(uint8_t *ptr, size_t len)
-{
-    // Increment elements right-to-left, stopping if there is no carry
-    while (len) if (++ptr[--len]) return true;
-    return false;
-}
-
 // Create an input differential randomly
 uint8_t *make_input_diff(size_t rounds, float l2pthresh)
 {
@@ -41,23 +40,19 @@ uint8_t *make_input_diff(size_t rounds, float l2pthresh)
     static uint8_t sched[16];
 
     // Randomly assign differences for last three words
-    for (int idx = 0; idx < 8; idx++)
-    {
-        sched[idx] = (idx < 4)? 0 : rand();
-    }
-    // If not viable, mutate slightly
-    while (!is_viable(sched, rounds, l2pthresh, 8, 0)) succ(sched, 8) ;
+    do for (int idx = 0; idx < 8; idx++) sched[idx] = (idx < 4)? 0 : csprng_rand();
+    while (!is_viable(sched, rounds, l2pthresh, 8, 0)) ;
     return sched;
 }
 
 // Entry point
 int main(int argc, char **argv)
 {
-    const float pthresh = -4.000000f;
-    const size_t rounds = 16;
-    const char *key_fname = "/Scratch/key-file-4.000000.bin",
-               *add_fname = "/Scratch/add-file-4.000000.bin",
-               *maj_fname = "/Scratch/maj-file-4.000000.bin";
+    const float pthresh = -3.000000f;
+    const size_t rounds = 12;
+    const char *key_fname = "/Scratch/key-file-3.000000.bin",
+               *add_fname = "/Scratch/add-file-3.000000.bin",
+               *maj_fname = "/Scratch/maj-file-3.000000.bin";
 
     log(stdout, "Initializing...");
     if (load_key_memo(key_fname)) log(stdout, "Loaded key memos from %s", key_fname);
@@ -66,39 +61,30 @@ int main(int argc, char **argv)
     else log(stdout, "Failed to load add memos from %s", add_fname);
     if (load_maj_memo(maj_fname)) log(stdout, "Loaded maj memos from %s", maj_fname);
     else log(stdout, "Failed to load maj memos from %s", maj_fname);
-    log(stdout, "Done!");
-
+    log(stdout, "Done!\n");
+    log(stdout, "Rounds: %zu/16", rounds);
+    log(stdout, "Threshold probability: 2^%f\n", pthresh);
+ 
     // Init PRNG
     unsigned int seed = time(NULL);
     srand(seed);
     do
     {
         // Reseed so we can deterministically perform a propagation
-        seed = rand();
+        seed = rand(); // csprng_rand();
         srand(seed);
 
         // Build an input differential, print it out
         uint8_t *diff = make_input_diff(rounds, pthresh);
-        log(stdout, "Fingerprint:");
-        for (int i = 0; i < 8; i++)
-        {
-            #define C2B(c) ((c)? 'x' : '-')
-            log(stdout, "%c%c%c%c%c%c%c%c", 
-                    C2B(diff[i] & 0x80),
-                    C2B(diff[i] & 0x40),
-                    C2B(diff[i] & 0x20),
-                    C2B(diff[i] & 0x10),
-                    C2B(diff[i] & 0x08),
-                    C2B(diff[i] & 0x04),
-                    C2B(diff[i] & 0x02),
-                    C2B(diff[i] & 0x01));
-            #undef C2B
-        }
-        log(stdout, "Seed: %u", seed);
-        log(stdout, "Rounds: %zu/16", rounds);
-        log(stdout, "Threshold probability: 2^%f", pthresh);
         std::pair<size_t, size_t> result = propagate(diff, rounds, pthresh);
-        log(stdout, "Trails: %zu/%zu\n", result.first, result.second);
+        if (result.first)
+        {
+            log(stdout, "Fingerprint: %02x%02x%02x%02x%02x%02x%02x%02x",
+                    diff[0], diff[1], diff[2], diff[3],
+                    diff[4], diff[5], diff[6], diff[7]);
+            log(stdout, "Seed: %u", seed);
+            log(stdout, "Trails: %zu/%zu\n", result.first, result.second);
+        }
     }
     while (1);
     return 0;
